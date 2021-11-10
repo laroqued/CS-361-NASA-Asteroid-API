cf06b11e88c93bb1c7a1ef9a37410b47f6ffec91
donnyves <donnyves@gmail.com>
2021-11-10 13:00:39 -0800

    uncomment nasa_api

--- app.js
+++ app.js
@@ -36,29 +36,29 @@ API_KEY = "ahutoSbmiRJp7CUBrL5s2yefa7OrVrKw1zsel4VF";
 startDate = "2021-10-01";
 endDate = "2021-10-02";
 
-(async() => {
-    try {
-        const response = await axios.get(
-            `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`
-        );
+// (async() => {
+//     try {
+//         const response = await axios.get(
+//             `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`
+//         );
 
-        console.log(response.data.url);
-        console.log(response.data.near_earth_objects);
-    } catch (error) {
-        console.log(error.response.body);
-    }
-})();
+//         //  console.log(response.data.url);
+//         // console.log(response.data.near_earth_objects);
+//     } catch (error) {
+//         console.log(error.response.body);
+//     }
+// })();
 
-const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`;
-axios({
-        method: "get",
-        url: url,
-        responseType: "stream",
-    })
-    .then((res) => {
-        res.data.pipe(fs.createWriteStream("nasa_api.json"));
-    })
-    .catch((err) => console.log(err));
+// const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`;
+// axios({
+//         method: "get",
+//         url: url,
+//         responseType: "stream",
+//     })
+//     .then((res) => {
+//         res.data.pipe(fs.createWriteStream("nasa_api.json"));
+//     })
+//     .catch((err) => console.log(err));
 
 //=========================================================================
 // function downloadURL(url, name) {
